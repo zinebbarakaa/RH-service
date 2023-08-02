5034b26eda5a1a78c7687cb0628c3a9ceeb2f0b0
zinebbarakaa <zinebbarakaa@gmail.com>
2023-08-02 12:28:14 +0100

    added postman file

--- src/main/java/com/giantLink/RH/controllers/RoleController.java
+++ src/main/java/com/giantLink/RH/controllers/RoleController.java
@@ -6,10 +6,9 @@ import com.giantLink.RH.services.RoleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/role")
@@ -21,5 +20,10 @@ public class RoleController {
     @PostMapping
     public ResponseEntity<RoleResponse> add (@RequestBody RoleRequest request){
         return  new ResponseEntity<>(roleService.add(request), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoleResponse>> getAllRoles(){
+        return new ResponseEntity<>(roleService.get(),HttpStatus.OK);
     }
 }
